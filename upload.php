5f312a44ed9676af8f214dfeca39e21eddeb4582
ArthurMaximino <39442709+ArthurMaximino@users.noreply.github.com>
2018-06-19 00:21:41 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,51 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>
+			LocUploader
+        </title>
+        <link rel="shortcut icon" href="favicon.ico">
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
+		<style>
+		body {
+     background: #FAFAFA;
+		}
+		.center {
+    margin: auto;
+    width: 50%;
+    padding: 70px;
+}
+	</style>
+	</head>
+	<body>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+  			<a class="navbar-brand" href="#">LocUploader</a>
+  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    			<span class="navbar-toggler-icon"></span>
+  			</button>
+  			<div class="collapse navbar-collapse" id="navbarNav">
+    			<ul class="navbar-nav ml-auto">
+      				<li class="nav-item">
+        				<a class="nav-link"><?php error_reporting(0); include("validacao.php"); echo $_SESSION['email']; ?></a>
+      				</li>
+      				<li class="nav-item">
+        				<a class="nav-link" href="fim.php">Sair</a>
+      				</li>
+    			</ul>
+  			</div>
+		</nav>
+
+<div class="center">
+		<div class="card border-dark mb-3">
+	<div class="card" >
+        <div class="card-body">
+
+
 <?php
+error_reporting(0);
 include_once("validacao.php");
 $email = $_SESSION['email'];
 
@@ -40,7 +87,11 @@ $nome_banco = "locuploader";
 $conecta = new mysqli($nome_servidor, $nome_usuario, $senha, $nome_banco);
 if ($conecta->connect_error)
 die("Ocorreu uma falha na conexão". $conecta->connect_error."<br>");
-mysqli_query($conecta,"INSERT INTO foto (nome_de_caminho,email) VALUES ('$arquivo_nome','$email')");
+$insere = mysqli_query($conecta,"INSERT INTO foto (nome_de_caminho,email) VALUES ('$arquivo_nome','$email')");
+if(!$insere)
+    die("Query Failed: " .  mysqli_error($conecta));
+else
+echo "Upload realizado com sucesso!";
 
 
 
@@ -48,3 +99,13 @@ mysqli_query($conecta,"INSERT INTO foto (nome_de_caminho,email) VALUES ('$arquiv
 
         
 ?>
+
+</p>
+<a href="home.php"><button class="btn btn-dark">Retornar para a página inicial</button></a>
+</div>
+</div>
+</div>
+</div>
+
+</body>
+</html>

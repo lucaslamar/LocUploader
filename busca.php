d804a196046b1b9d001c36b3efd32d03b936d95a
Lucas Lamar <lucass.lamar@gmail.com>
2018-06-20 21:10:54 -0300

    fe no pai que agora vai

--- busca.php
+++ busca.php
@@ -4,82 +4,77 @@
 		<meta charset="utf-8">
 		<title>
 			LocUploader
-		</title>
-		<link rel="shortcut icon" href="favicon.ico">
+        </title>
+        <link rel="shortcut icon" href="favicon.ico">
+
 		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
 		<style>
 		body {
-     background: grey;
+     background: #FAFAFA;
 		}
 		.center {
     margin: auto;
     width: 50%;
     padding: 70px;
 }
+
 	</style>
 	</head>
-	<body bgcolor="#E6E6FA">
-
-		<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
-  			<a class="navbar-brand" href="#">LocUploader</a>
+	<body>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+  			<a class="navbar-brand" href="home.php">LocUploader</a>
   			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     			<span class="navbar-toggler-icon"></span>
   			</button>
   			<div class="collapse navbar-collapse" id="navbarNav">
     			<ul class="navbar-nav ml-auto">
       				<li class="nav-item">
-        				<a class="nav-link" href="#">Novo Usuário</a>
+        				<a class="nav-link"><?php error_reporting(0); include("validacao.php"); echo $_SESSION['email']; ?></a>
       				</li>
       				<li class="nav-item">
-        				<a class="nav-link" href="#">Sair</a>
+        				<a class="nav-link" href="fim.php">Sair</a>
       				</li>
     			</ul>
   			</div>
-		</nav>
+        </nav>
 
-		<div class="card bg-dark text-black">
-  <img class="card-img" src="http://in-spaces.com/images/detailed/23/CROP_WOODEN_PLANKS-_WHITE_1024x1024.jpg?t=1468185097" alt="Card image cap">
-  <div class="card-img-overlay">
-			
-
-
-	<div class="center">
+<div class="center">
 		<div class="card border-dark mb-3">
 	<div class="card" >
         <div class="card-body">
-		<form action="validacao.php" method="POST">
-		<div class="form-group">
+        <?php 
+include("validacao.php");
+session_start();
+            
+$nome_servidor = "127.0.0.1";
+$nome_usuario = "root";
+$senha = "";
+$nome_banco = "locuploader";
+$conecta = new mysqli($nome_servidor, $nome_usuario, $senha, $nome_banco);
+if ($conecta->connect_error)
+die("Ocorreu uma falha na conexão". $conecta->connect_error."<br>");
 
-    		<table align="center">
+$identificador = $_POST["busca"];
+$_SESSION['busca'] = $identificador;
+$email = $_SESSION['email'];
+$sql = mysqli_query($conecta,"SELECT * FROM foto WHERE id_foto = '$identificador'"); 
+while($linha = mysqli_fetch_array($sql)) { 
+    echo "<img src= '".$linha['nome_de_caminho']."' class='img-thumbnail'>";
 
-    			<tr>
+}
 
-    				<td>E-mail:&nbsp;&nbsp;</td>
+?>
 
-    				<td><input type ="text" name="email" class="form-control-plaintext" placeholder="Entre com seu e-mail" required></td>
+        
+		<p></p>
+        <a href="excluir.php"><button class="btn btn-primary btn-lg btn-block">Apagar foto</button></a>
+        <p></p>
+        <a href="pesquisar.php"><button class="btn btn-primary btn-lg btn-block">Retornar</button></a>
 
-    			</tr>
 
-    			<tr>
-
-    				<td>Senha:&nbsp;&nbsp;</td>
-
-    				<td><input type="password" name="senha" class="form-control-plaintext" placeholder="Entre com a sua senha" required></td>
-
-				</tr>
-    			<tr>
-
-    				<td colspan="2" align="center"><input type="submit" value="Login" name="login" class="btn btn-dark"></td>
-    			</tr>
-
-    		</table>
-    	</div>
-		</form>
-		<a href="cadastro.html"><p>Cadastre-se!</p></a>
-    </div>
-    </div>
+</div>
 </div>
 </div>
 </div>
--- excluir.php
+++ excluir.php
@@ -6,7 +6,8 @@
 			LocUploader
         </title>
         <link rel="shortcut icon" href="favicon.ico">
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+		<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
 		<style>
@@ -18,6 +19,7 @@
     width: 50%;
     padding: 70px;
 }
+
 	</style>
 	</head>
 	<body>
@@ -36,32 +38,14 @@
       				</li>
     			</ul>
   			</div>
-		</nav>
+        </nav>
 
 <div class="center">
 		<div class="card border-dark mb-3">
 	<div class="card" >
         <div class="card-body">
-        <p align="center">Bem-vindo, 
-            <?php error_reporting(0); 
-            include("validacao.php");
-            $nome_servidor = "127.0.0.1";
-            $nome_usuario = "root";
-            $senha = "";
-            $nome_banco = "locuploader";
-            $conecta = new mysqli($nome_servidor, $nome_usuario, $senha, $nome_banco);
-            if ($conecta->connect_error)
-            die("Ocorreu uma falha na conexão". $conecta->connect_error."<br>");
-            $email = $_SESSION['email'];
-            $nome = mysqli_query($conecta,"SELECT nome_usuario FROM usuario WHERE email='$email'");
-            $resultado = mysqli_fetch_assoc($nome);
-            echo $resultado["nome_usuario"];
-                
-            ?>!</p><br>
-        <p align="center">Fotos armazenadas:
             <?php
-            error_reporting(0);
-            include("validacao.php");
+            session_start();
             
             $nome_servidor = "127.0.0.1";
             $nome_usuario = "root";
@@ -70,35 +54,26 @@
             $conecta = new mysqli($nome_servidor, $nome_usuario, $senha, $nome_banco);
             if ($conecta->connect_error)
             die("Ocorreu uma falha na conexão". $conecta->connect_error."<br>");
-            $email = $_SESSION['email'];
-            $teste = mysqli_query($conecta,"SELECT COUNT(id_foto) as total FROM foto WHERE email='$email'");
-            $exibe = mysqli_fetch_assoc($teste);
-            echo $exibe["total"];
+$identificador = $_SESSION['busca'];
+            $exclui = mysqli_query($conecta,"DELETE FROM foto WHERE id_foto = '$identificador'");
+if(!$exclui)
+    die("Query Failed: " .  mysqli_error($conecta));
+else
+echo "Foto excluída com sucesso!";
+
+
+
+    
+
+
             ?>
-        </p><br>
-		<div class="form-group">
-            
-            <table align="center">
-            <tr>
-                <td>
-                <a href="site.php"><button class="btn btn-dark">Upload</button></a>
-                </td>
-                <td>
-                    <a href="pesquisar.php"><button class="btn btn-dark">Pesquisar</button></a>
-                </td>
-            </tr>
-            <tr>
-                <td>
-
-                </td>
-            </tr>
 
 
-            </table>
-
+<a href="home.php"><button class="btn btn-dark">Retornar para a página inicial</button></a>
 
 </div>
-    	
-    </div>
-    </div>
 </div>
+</div>
+</div>
+</body>
+</html>
